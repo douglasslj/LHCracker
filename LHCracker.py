fba170fa4278ed27e6297f4580c9847ca0b8d2c0
douglasslj <douglaslopes.dlj@gmail.com>
2020-03-25 21:21:07 -0300

    upload

---
+++ LHCracker.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import crypt
+
+print ""
+print "Bem vindo ao Linux Hash Cracker"
+print ""
+
+fullHash = raw_input("Informe a hash de senha Linux completa: ")
+wordlist = raw_input("Informe a Wordlist: ")
+salt = fullHash[:12]
+    
+with open(wordlist, "rU") as infile:
+    for line in infile:
+        line = line.strip()
+        hashGenerated = crypt.crypt(line,salt)
+        
+        if hashGenerated == fullHash:
+            print ""
+            print " -- Hash quebrada --"
+            print ""
+            print "Senha:",line
+            print ""
+            break
+        else:
+            print "Verificando:",line
+        
+        
+
